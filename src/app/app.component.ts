e442fafe6f872d1b912425b9beb0dee8551f283f
bioDanCho <daniel.cho@thebioagency.com>
2022-06-17 15:50:49 -0400

    generate teams logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-team-app';
+    newMemberName: string = '';
+    members: string[] = [];
+    errorMessage: string = '';
+    numberOfTeams: number | '' = '';
+    teams: string[][] = [];
+
+    onInput(member: string): void {
+        this.newMemberName = member;
+    }
+
+    onNumberOfTeamsInput(value: string): void {
+        this.numberOfTeams = +value;
+    }
+
+    addMember(): void {
+        if (!this.newMemberName) {
+            this.errorMessage = 'Name cannot be empty!';
+            return;
+        }
+
+        this.errorMessage = '';
+        this.members.push(this.newMemberName);
+        this.newMemberName = '';
+    }
+
+    generateTeams() {
+        if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+            this.errorMessage = 'Invalid number of teams!';
+            return;
+        }
+
+        if (this.members.length < this.numberOfTeams) {
+            this.errorMessage = 'Not enough members!';
+            return;
+        }
+
+        this.errorMessage = '';
+        const allMembers = [...this.members];
+
+        while (allMembers.length) {
+            for (let i = 0; i < this.numberOfTeams; i++) {
+                const randomIndex = Math.floor(
+                    Math.random() * allMembers.length
+                );
+                const member = allMembers.splice(randomIndex, 1)[0];
+
+                if (!member) {
+                    break;
+                }
+
+                if (this.teams[i]) {
+                    this.teams[i].push(member);
+                } else {
+                    this.teams[i] = [member];
+                }
+            }
+        }
+
+        this.members = [];
+        this.numberOfTeams = '';
+    }
 }
